3729cce04bb48d3e63d66ec3b27a0cd21feccf4e
Ivan Bukirev <i.bukirev@gmail.com>
2025-03-14 21:45:35 +0300

    modif

---
+++ classes/working_with_files_3.py
@@ -0,0 +1,17 @@
+def merge_files(file_names, output_file):
+    file_contents = []
+    for file_name in file_names:
+        with open(file_name, 'r', encoding='utf-8') as file:
+            content = file.readlines()
+            file_contents.append((file_name, len(content), content))
+    file_contents.sort(key=lambda x: x[1])
+    with open(output_file, 'w', encoding='utf-8') as file:
+        for file_name, count, content in file_contents:
+            file.write(f"\n{file_name}\n{count}\n")
+            file.writelines(content)
+
+
+
+file_names = ['1.txt', '2.txt', '3.txt']
+output_file = '4.txt'
+merge_files(file_names, output_file)
