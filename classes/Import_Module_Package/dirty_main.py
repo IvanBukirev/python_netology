536479c2f625650bbe45af42dab9e0488f327888
Ivan Bukirev <i.bukirev@gmail.com>
2025-04-25 01:01:43 +0300

    Import.Module.Package

--- classes/Import_Module_Package/dirty_main.py
+++ classes/Import_Module_Package/dirty_main.py
@@ -2,6 +2,7 @@ from classes.Import_Module_Package.main import *
 
 
 print(__name__)
+print(f'Текущая дата:{datetime.today().date()}')
 calculate_salary()
 get_employees()
 print(rich_example.__doc__)
